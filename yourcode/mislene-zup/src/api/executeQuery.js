7cb48f2ca05d037ebb6f6f213a8a1267fb610386
Mislene <mislenedalila@gmail.com>
2020-03-29 22:55:17 -0300

    versao final ok

--- yourcode/mislene-zup/src/api/executeQuery.js
+++
@@ -1,8 +0,0 @@
-import Const from '@/helpers/const'
-import { custAxios } from './genericApi'
-
-export default {
-  executeQuery (body) {
-    return custAxios().post(`${Const.API_EXECUTE_QUERY}`, body)
-  }
-}
