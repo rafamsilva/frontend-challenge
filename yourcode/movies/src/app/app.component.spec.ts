96391c4405da0ca0758cf01bf4e8c62d48c05d40
Vinicius Vense <viniciusvense@gmail.com>
2020-04-14 17:41:05 -0300

    Remove Home spec

--- yourcode/movies/src/app/app.component.spec.ts
+++ yourcode/movies/src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('movies');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('movies app is running!');
-  });
 });
