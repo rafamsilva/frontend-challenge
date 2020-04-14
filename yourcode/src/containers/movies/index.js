ea3a3a840fb8373280a48efb431e40c8058c3046
Darwin Boaventura <darwinboaventura@gmail.com>
2020-04-14 10:55:55 -0300

    feat: wip home page

--- yourcode/src/containers/movies/index.js
+++ yourcode/src/containers/movies/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
-import { Container } from './style';
+import { Container, MovieGrid } from './style';
+
 import MoviesGridItem from '../../components/movies-grid-item';
+import NothingFound from '../../components/nothing-found';
 
 export default function MoviesContainer({ movies }) {
   return (
     <Container>
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
-      <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+      {movies ? (
+        <MovieGrid>
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+          <MoviesGridItem image="https://www.fillmurray.com/400/400" title="What We Do in the Shadows" year="2018" />
+        </MovieGrid>
+      ) : (
+        <NothingFound />
+      )}
     </Container>
   );
 }
--- yourcode/src/containers/movies/style.js
+++ yourcode/src/containers/movies/style.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.main`
   margin-top: 32px;
+`;
 
+export const MovieGrid = styled.section`
   display: grid;
   grid-template-columns: repeat(6, 15.254237288%);
   grid-auto-rows: 240px;
