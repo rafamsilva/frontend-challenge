ac3e5000a1d9fd17a0a64d8eaced90d6066afb05
Darwin Boaventura <darwinboaventura@gmail.com>
2020-04-16 08:48:13 -0300

    fix: refactor badge component test

--- yourcode/src/components/badge/__tests__/index.test.js
+++ yourcode/src/components/badge/__tests__/index.test.js
@@ -20,31 +20,33 @@ describe('Badge Component', () => {
     container = null;
   });
 
-  it('should render without crashing', () => {
-    act(() => {
-      render(<Badge value="10" />, container);
+  describe('Render', () => {
+    it('should render without crashing', () => {
+      act(() => {
+        render(<Badge value="10" />, container);
+      });
+    });
+
+    it('should render without an icon', () => {
+      act(() => {
+        render(<Badge value="10" />, container);
+      });
+
+      expect(container.querySelector("[data-testid='icon']")).toBeNull();
+      expect(container.querySelector("[data-testid='text']").textContent).toBe('10');
+    });
+
+    it('should render with an icon', () => {
+      act(() => {
+        render(<Badge value="10" icon={<ImdbLogo alt="IMDB Logo" title="IMDB Logo" />} />, container);
+      });
+
+      expect(container.querySelector("[data-testid='icon']")).toBeTruthy();
+      expect(container.querySelector("[data-testid='text']").textContent).toBe('10');
     });
   });
 
-  it('should render without an icon', () => {
-    act(() => {
-      render(<Badge value="10" />, container);
-    });
-
-    expect(container.querySelector("[data-testid='icon']")).toBeNull();
-    expect(container.querySelector("[data-testid='text']").textContent).toBe('10');
-  });
-
-  it('should render with an icon', () => {
-    act(() => {
-      render(<Badge value="10" icon={<ImdbLogo alt="IMDB Logo" title="IMDB Logo" />} />, container);
-    });
-
-    expect(container.querySelector("[data-testid='icon']")).toBeTruthy();
-    expect(container.querySelector("[data-testid='text']").textContent).toBe('10');
-  });
-
-  describe('Should match snapshot', () => {
+  describe('Snapshot', () => {
     it('should match snapshot without icon', () => {
       act(() => {
         render(<Badge value="10" />, container);
