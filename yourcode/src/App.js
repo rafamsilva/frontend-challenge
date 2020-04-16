e38136b665efe46f553a22e4489cc4dd01de0e2f
Darwin Boaventura <darwinboaventura@gmail.com>
2020-04-16 02:19:28 -0300

    feat: added state persistency

--- yourcode/src/App.js
+++ yourcode/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import LoaderContainer from './containers/loader';
 
@@ -13,19 +14,21 @@ import MoviePage from './pages/movie';
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <LoaderContainer />
+      <PersistGate loading={<LoaderContainer active={true} />} persistor={persistor}>
+        <Router>
+          <LoaderContainer />
 
-        <Switch>
-          <Route path="/movie/:id">
-            <MoviePage />
-          </Route>
+          <Switch>
+            <Route path="/movie/:id">
+              <MoviePage />
+            </Route>
 
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </Router>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- yourcode/src/store/index.js
+++ yourcode/src/store/index.js
@@ -1,11 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import reducer from './reducer';
 import { fetchMoviesSaga } from './sagas';
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  blacklist: ['loading'],
+};
+
 const sagaMiddleware = createSagaMiddleware();
 
-export default createStore(reducer, applyMiddleware(sagaMiddleware));
+const pReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(pReducer, applyMiddleware(sagaMiddleware));
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(fetchMoviesSaga);
